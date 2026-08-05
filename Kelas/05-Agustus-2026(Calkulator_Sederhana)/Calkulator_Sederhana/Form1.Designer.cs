e6e4715a36d42664d4ca1f62712788bc9eebf3f3
n4uf4l~17 <naufalsururi10@gmail.com>
2026-08-05 13:07:51 +0700

    Add files via upload

---
+++ Kelas/05-Agustus-2026(Calkulator_Sederhana)/Calkulator_Sederhana/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Calkulator_Sederhana
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TambahBtn = new System.Windows.Forms.Button();
+            this.lblHasil = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblAngka1 = new System.Windows.Forms.Label();
+            this.lblAngka2 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.TextBox1 = new System.Windows.Forms.TextBox();
+            this.TextBox2 = new System.Windows.Forms.TextBox();
+            this.CalkulatorSederhana = new System.Windows.Forms.Label();
+            this.KurangBtn = new System.Windows.Forms.Button();
+            this.KaliBtn = new System.Windows.Forms.Button();
+            this.BagiBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TambahBtn
+            // 
+            this.TambahBtn.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TambahBtn.Location = new System.Drawing.Point(12, 226);
+            this.TambahBtn.Name = "TambahBtn";
+            this.TambahBtn.Size = new System.Drawing.Size(356, 27);
+            this.TambahBtn.TabIndex = 2;
+            this.TambahBtn.Text = "Tambah";
+            this.TambahBtn.UseVisualStyleBackColor = true;
+            this.TambahBtn.Click += new System.EventHandler(this.TambahBtn_Click);
+            // 
+            // lblHasil
+            // 
+            this.lblHasil.AutoSize = true;
+            this.lblHasil.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHasil.Location = new System.Drawing.Point(12, 180);
+            this.lblHasil.Name = "lblHasil";
+            this.lblHasil.Size = new System.Drawing.Size(47, 17);
+            this.lblHasil.TabIndex = 0;
+            this.lblHasil.Text = "Hasil :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(171, 206);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 17);
+            this.label4.TabIndex = 0;
+            // 
+            // lblAngka1
+            // 
+            this.lblAngka1.AutoSize = true;
+            this.lblAngka1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAngka1.Location = new System.Drawing.Point(12, 51);
+            this.lblAngka1.Name = "lblAngka1";
+            this.lblAngka1.Size = new System.Drawing.Size(51, 17);
+            this.lblAngka1.TabIndex = 0;
+            this.lblAngka1.Text = "Angka1";
+            // 
+            // lblAngka2
+            // 
+            this.lblAngka2.AutoSize = true;
+            this.lblAngka2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAngka2.Location = new System.Drawing.Point(12, 109);
+            this.lblAngka2.Name = "lblAngka2";
+            this.lblAngka2.Size = new System.Drawing.Size(51, 17);
+            this.lblAngka2.TabIndex = 0;
+            this.lblAngka2.Text = "Angka2";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(171, 206);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 17);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Aksi";
+            // 
+            // TextBox1
+            // 
+            this.TextBox1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBox1.Location = new System.Drawing.Point(12, 71);
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.Size = new System.Drawing.Size(356, 25);
+            this.TextBox1.TabIndex = 1;
+            // 
+            // TextBox2
+            // 
+            this.TextBox2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBox2.Location = new System.Drawing.Point(12, 129);
+            this.TextBox2.Name = "TextBox2";
+            this.TextBox2.Size = new System.Drawing.Size(356, 25);
+            this.TextBox2.TabIndex = 1;
+            // 
+            // CalkulatorSederhana
+            // 
+            this.CalkulatorSederhana.AutoSize = true;
+            this.CalkulatorSederhana.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CalkulatorSederhana.Location = new System.Drawing.Point(88, 9);
+            this.CalkulatorSederhana.Name = "CalkulatorSederhana";
+            this.CalkulatorSederhana.Size = new System.Drawing.Size(203, 25);
+            this.CalkulatorSederhana.TabIndex = 0;
+            this.CalkulatorSederhana.Text = "Calkulator Sederhana";
+            // 
+            // KurangBtn
+            // 
+            this.KurangBtn.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.KurangBtn.Location = new System.Drawing.Point(12, 259);
+            this.KurangBtn.Name = "KurangBtn";
+            this.KurangBtn.Size = new System.Drawing.Size(356, 27);
+            this.KurangBtn.TabIndex = 2;
+            this.KurangBtn.Text = "Kurang";
+            this.KurangBtn.UseVisualStyleBackColor = true;
+            this.KurangBtn.Click += new System.EventHandler(this.KurangBtn_click);
+            // 
+            // KaliBtn
+            // 
+            this.KaliBtn.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.KaliBtn.Location = new System.Drawing.Point(12, 292);
+            this.KaliBtn.Name = "KaliBtn";
+            this.KaliBtn.Size = new System.Drawing.Size(356, 27);
+            this.KaliBtn.TabIndex = 2;
+            this.KaliBtn.Text = "Kali";
+            this.KaliBtn.UseVisualStyleBackColor = true;
+            this.KaliBtn.Click += new System.EventHandler(this.KaliBtn_click);
+            // 
+            // BagiBtn
+            // 
+            this.BagiBtn.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BagiBtn.Location = new System.Drawing.Point(12, 325);
+            this.BagiBtn.Name = "BagiBtn";
+            this.BagiBtn.Size = new System.Drawing.Size(356, 27);
+            this.BagiBtn.TabIndex = 2;
+            this.BagiBtn.Text = "Bagi";
+            this.BagiBtn.UseVisualStyleBackColor = true;
+            this.BagiBtn.Click += new System.EventHandler(this.BagiBtn_click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 361);
+            this.Controls.Add(this.BagiBtn);
+            this.Controls.Add(this.KaliBtn);
+            this.Controls.Add(this.KurangBtn);
+            this.Controls.Add(this.TambahBtn);
+            this.Controls.Add(this.TextBox2);
+            this.Controls.Add(this.TextBox1);
+            this.Controls.Add(this.CalkulatorSederhana);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblAngka2);
+            this.Controls.Add(this.lblHasil);
+            this.Controls.Add(this.lblAngka1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button TambahBtn;
+        private System.Windows.Forms.Label lblHasil;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblAngka1;
+        private System.Windows.Forms.Label lblAngka2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox TextBox1;
+        private System.Windows.Forms.TextBox TextBox2;
+        private System.Windows.Forms.Label CalkulatorSederhana;
+        private System.Windows.Forms.Button KurangBtn;
+        private System.Windows.Forms.Button KaliBtn;
+        private System.Windows.Forms.Button BagiBtn;
+    }
+}
+
---
+++ Kelas/05-Agustus-2026(Calkulator_Sederhana)/Calkulator_Sederhana/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calkulator_Sederhana
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TambahBtn_Click(object sender, EventArgs e)
+        {
+            int angka1 = Convert.ToInt32(TextBox1.Text);
+            int angka2 = Convert.ToInt32(TextBox2.Text);
+
+            int penjumlahan = angka1 + angka2;
+
+            lblHasil.Text = "Hasil : " + penjumlahan.ToString();
+
+        }
+
+        private void KurangBtn_click(object sender, EventArgs e)
+        {
+
+            int angka1 = Convert.ToInt32(TextBox1.Text);
+            int angka2 = Convert.ToInt32(TextBox2.Text);
+
+            int pengurangan = angka1 - angka2;
+
+            lblHasil.Text = pengurangan.ToString();
+
+        }
+
+        private void KaliBtn_click(object sender, EventArgs e)
+        {
+
+            int angka1 = Convert.ToInt32(TextBox1.Text);
+            int angka2 = Convert.ToInt32(TextBox2.Text);
+
+            int perkalian = angka1 * angka2;
+
+            lblHasil.Text = perkalian.ToString();
+
+        }
+
+        private void BagiBtn_click(object sender, EventArgs e)
+        {
+
+            int angka1 = Convert.ToInt32(TextBox1.Text);
+            int angka2 = Convert.ToInt32(TextBox2.Text);
+
+            if (angka2 == 0)
+            {
+                MessageBox.Show("Tidak Bisa Membagi Dengan Angka 0!");
+                return;
+            }
+
+            double pembagian = (double)angka1 / angka2;
+
+            lblHasil.Text = pembagian.ToString();
+
+        }
+
+    }
+}
