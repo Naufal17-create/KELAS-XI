9c59508cd21ea308b119e7e6f4cdeebd996ea2d5
n4uf4l~17 <naufalsururi10@gmail.com>
2026-08-26 09:40:15 +0700

    Add files via upload

---
+++ Kelas/12-Agustus-2026(DesignForm1-3)/Design/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Design
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void formlogin_Click_1(object sender, EventArgs e)
+        {
+            Form3Dashboard dashboard = new Form3Dashboard();
+            this.Hide();
+            dashboard.Show();
+        }
+
+        private void DaftarAkun_Click(object sender, EventArgs e)
+        {
+            Form2Register register = new Form2Register();
+            this.Hide();
+            register.Show();
+        }
+    }
+}
---
+++ Kelas/12-Agustus-2026(DesignForm1-3)/Design/Form3Dashboard.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Design
+{
+    public partial class Form3Dashboard : Form
+    {
+        public Form3Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void LogOut_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox9_Click(object sender, EventArgs e)
+        {
+            Form1 login = new Form1();
+            this.Close();
+            login.Show();
+        }
+    }
+}
